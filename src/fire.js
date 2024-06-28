4899f6b014d1f82aec1bb5cabf2e33164e6c9cf4
Nazar Iskenov <iskenovnazar0@gmail.com>
2024-06-29 00:37:02 +0600

    хз

--- src/fire.js
+++ src/fire.js
@@ -1,4 +1,3 @@
-
 // Import the functions you need from the SDKs you need
 // import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -10,15 +9,6 @@ import firebase from "firebase/compat/app";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAo4_aopFBWsZuKiTbtIMx03gkXRD7wU9w",
-  authDomain: "shop-js39-938ec.firebaseapp.com",
-  projectId: "shop-js39-938ec",
-  storageBucket: "shop-js39-938ec.appspot.com",
-  messagingSenderId: "973101634745",
-  appId: "1:973101634745:web:06133bbde93dcb9c53b768",
-  measurementId: "G-K9PZY4N6GQ",
-};
 
 // Initialize Firebase
 const fire = firebase.initializeApp(firebaseConfig);
@@ -37,5 +27,3 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export default firebaseConfig;
-
